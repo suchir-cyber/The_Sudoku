58f5f051611143fc177d982b8b3ff2efa49abe3d
suchir-cyber <suchirpandula@gmail.com>
2024-02-03 00:18:16 +0530

    updated visualization part

--- script.js
+++ script.js
@@ -19,3 +19,50 @@ document.addEventListener("DOMContentLoaded", function(){
         sudokugrid.appendChild(newRow);
     }
 });
+
+async function solveSudoku(){
+    const gridSize = 0;
+    const sudokuArray = [];
+
+    //filling the sudokuArray
+    for(let row=0;row<gridSize;row++){
+        sudokuArray[row] = [];
+        for(let col=0;col<gridSize;col++){
+            const cellId = `cell-${row}-${col}`;
+            const cellValue = document.getElementById(cellId).ariaValueMax;
+            sudokuArray[row][col] = cellValue !== ""? parseInt(cellValue) : 0;
+        }
+    }
+
+    // Identifying user-input cells and mark them
+    for(let row=0;row<gridSize;row++){
+        for(let col=0;col<gridSize;col++){
+            const cellId = `cell-${row}-${col}`;
+            const cell = document.getElementById(cellId);
+            if(sudoku[row][col] != 0){
+                cell.classList.add("user-input");
+            }
+        }
+    }
+
+    //solve the sudoku and display the solution
+
+    if(solveSudokuHelper(sudokuArray)){
+        for(let row=0;row<gridSize;row++){
+            for(let col=0;col<gridSize;col++){
+                const cellId = `cell-${row}-${col}`;
+                const cell = document.getElementById(cellId);
+
+                // fill in the solved values and apply animation
+                if(!cell.classList.contains("user-input")){
+                    cell.value = sudokuArray[row][col];
+                    cell.classList.add("solved");
+                    await sleep(20); //adding delay for visulization
+                }
+            }
+        }
+    }
+    else{
+        alert("No solution exists for the given sudoku puzzle...")
+    }
+}
